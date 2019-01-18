825849e4f9b46941b59f63c6088ca5f84e865b33
Dimitar Dimitrov <antraks92@gmail.com>
2019-01-18 09:42:50 +0200

    Upload

---
+++ Lab: Document Object Model/01_Articles-List/solution.js
@@ -0,0 +1,24 @@
+function solve() {
+
+    var title = document.getElementById('createTitle').value;
+    var text = document.getElementById('createContent').value;
+
+    if (title.length > 0 && text.length > 0) {
+        var article = document.createElement('article');
+        var h3 = document.createElement('h3');
+        var p = document.createElement('p');
+
+        h3.textContent = title;
+        p.textContent = text;
+
+        article.appendChild(h3);
+        article.appendChild(p);
+
+        //Append h3 and p to #articles
+        document.getElementById('articles').appendChild(article);
+
+        //Clear title and textarea content
+        document.getElementById('createTitle').value = '';
+        document.getElementById('createContent').value = '';
+    }
+}
