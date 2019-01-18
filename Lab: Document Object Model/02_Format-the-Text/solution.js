c3306fcbb0fb98287e5ddbeacfae7d9702d44652
Dimitar Dimitrov <antraks92@gmail.com>
2019-01-18 17:21:23 +0200

    Upload

--- Lab: Document Object Model/02_Format-the-Text/solution.js
+++ Lab: Document Object Model/02_Format-the-Text/solution.js
@@ -1,4 +1,11 @@
-function solve(){
-  //TODO
-  
+function solve() {
+    var sentences = document.getElementById('input').innerText
+        .split('.')
+        .filter(item => item);
+
+    for (i = 0; i < sentences.length; i++) {
+        var p = document.createElement('p');
+        p.textContent = sentences[i];
+        document.getElementById('output').appendChild(p);
+    }
 }
