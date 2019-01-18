2b4a0ac0fbef22417b1d149fdc2ea5ab077a0884
dimaldim <d4ti3ba>
2019-01-18 22:59:15 +0200

    Upload

--- Lab: Document Object Model/02_Format-the-Text/solution.js
+++ Lab: Document Object Model/02_Format-the-Text/solution.js
@@ -1,6 +1,21 @@
 function solve() {
-  var sentences = document
-    .getElementById("input")
-    .innerText.split(".")
-    .filter(item => item);
+  let input = document.getElementById('input');
+  let output = document.getElementById('output');
+
+  let sentences = input.textContent.split('.');
+
+  for (let i = 0; i < sentences.length; i += 3) {
+    let paragraph = document.createElement('p');
+    paragraph.textContent = sentences[i];
+
+    if (sentences.length > i + 1) {
+      paragraph.textContent += '. ' + sentences[i];
+    }
+
+    if (sentences.length > i + 2) {
+      paragraph.textContent += '. ' + sentences[i + 2];
+    }
+
+    output.appendChild(paragraph);
+  }
 }
