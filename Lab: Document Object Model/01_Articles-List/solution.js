085737338c5e0884a27bed91b173234b92f1d15b
Dimitar Dimitrov <antraks92@gmail.com>
2019-01-18 20:14:43 +0200

    Upload

--- Lab: Document Object Model/01_Articles-List/solution.js
+++ Lab: Document Object Model/01_Articles-List/solution.js
@@ -1,23 +1,23 @@
 function solve() {
-    var title = document.getElementById('createTitle').value;
-    var text = document.getElementById('createContent').value;
+  var title = document.getElementById("createTitle").value;
+  var text = document.getElementById("createContent").value;
 
-    if (title.length > 0 && text.length > 0) {
-        var article = document.createElement('article');
-        var h3 = document.createElement('h3');
-        var p = document.createElement('p');
+  if (title.length > 0 && text.length > 0) {
+    var article = document.createElement("article");
+    var h3 = document.createElement("h3");
+    var p = document.createElement("p");
 
-        h3.textContent = title;
-        p.textContent = text;
+    h3.textContent = title;
+    p.textContent = text;
 
-        article.appendChild(h3);
-        article.appendChild(p);
+    article.appendChild(h3);
+    article.appendChild(p);
 
-        //Append <article> to #articles
-        document.getElementById('articles').appendChild(article);
+    //Append <article> to #articles
+    document.getElementById("articles").appendChild(article);
 
-        //Clear title and textarea content
-        document.getElementById('createTitle').value = '';
-        document.getElementById('createContent').value = '';
-    }
+    //Clear title and textarea content
+    document.getElementById("createTitle").value = "";
+    document.getElementById("createContent").value = "";
+  }
 }
--- Lab: Document Object Model/02_Format-the-Text/solution.js
+++ Lab: Document Object Model/02_Format-the-Text/solution.js
@@ -1,11 +1,6 @@
 function solve() {
-    var sentences = document.getElementById('input').innerText
-        .split('.')
-        .filter(item => item);
-
-    for (i = 0; i < sentences.length; i++) {
-        var p = document.createElement('p');
-        p.textContent = sentences[i];
-        document.getElementById('output').appendChild(p);
-    }
+  var sentences = document
+    .getElementById("input")
+    .innerText.split(".")
+    .filter(item => item);
 }
