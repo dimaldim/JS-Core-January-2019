4b663b4d452431c3137e848b11fe538be411123c
Dimitar <antraks92@gmail.com>
2019-01-23 10:32:50 +0200

    Upload

--- Exercise Functions and Logic Flow/02_Simple-Number-Validator/solution.js
+++ Exercise Functions and Logic Flow/02_Simple-Number-Validator/solution.js
@@ -1,3 +1,29 @@
 (function () {
-    //TODO...
+    let button = document.querySelector('#exercise button');
+    let weightPosition = [2, 4, 8, 5, 10, 9, 7, 3, 6];
+
+    button.addEventListener('click', checkNumber);
+
+    function checkNumber() {
+        let number = document.getElementsByTagName('input')[0];
+        let lastDigit = function () {
+            return Number(number.value) % 10;
+        };
+        let weightNums = function () {
+            let sum = 0;
+            for (let i = 0; i < number.value.length - 1; i++) {
+                sum += Number(number.value[i]) * weightPosition[i];
+            }
+            return sum;
+        };
+        let sumNums = function () {
+            return weightNums() % 11 === 10 ? 0 : weightNums() % 11;
+        };
+
+        if (lastDigit() === sumNums()) {
+            document.getElementById('response').textContent = 'This number is Valid!';
+        } else {
+            document.getElementById('response').textContent = 'This number is NOT Valid!';
+        }
+    }
 })();
