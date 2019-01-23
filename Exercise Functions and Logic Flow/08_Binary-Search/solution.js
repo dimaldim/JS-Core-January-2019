34f0a73f3a4b9af829fcbe89ff3f91a673371cf7
Dimitar <antraks92@gmail.com>
2019-01-23 16:08:42 +0200

    Upload

--- Exercise Functions and Logic Flow/08_Binary-Search/solution.js
+++ Exercise Functions and Logic Flow/08_Binary-Search/solution.js
@@ -1,3 +1,13 @@
 function binarySearch() {
-    //TODO...
+
+    let arr = document.getElementById('arr').value.split(', ');
+    let num = document.getElementById('num').value;
+    let resultElement = document.getElementById('result');
+
+    if (arr.indexOf(num) !== -1) {
+        resultElement.innerHTML = `Found ${num} at index ${arr.indexOf(num)}`;
+    } else {
+        resultElement.innerHTML = `${num} is not in the array`;
+    }
+
 }
