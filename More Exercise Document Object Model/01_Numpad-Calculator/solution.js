d54274113248563b564ebded096874af1e113a16
Dimitar <antraks92@gmail.com>
2019-01-23 09:08:05 +0200

    Upload

--- More Exercise Document Object Model/01_Numpad-Calculator/solution.js
+++ More Exercise Document Object Model/01_Numpad-Calculator/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-    // TODO
+    let buttons = Array.from(document.getElementsByTagName('button'));
+    let exprField = document.getElementById('expressionOutput');
+    let result = document.getElementById('resultOutput');
+
+    buttons.forEach((button) => {
+        if (button.textContent === '=') {
+            button.addEventListener('click', showResult);
+        } else if (button.textContent === 'Clear') {
+            button.addEventListener('click', clearContent);
+        } else {
+            button.addEventListener('click', add);
+        }
+    });
+
+    function showResult() {
+
+
+    }
+
+    function clearContent() {
+        exprField.textContent = '';
+        result.textContent = '';
+    }
+
+    function add(e) {
+        exprField.textContent += e.target.textContent;
+    }
+
 }
