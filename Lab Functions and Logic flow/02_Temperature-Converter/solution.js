0b492aae24cc4327987868489aa99a67b4eb6ac1
Dimitar <antraks92@gmail.com>
2019-01-23 09:26:55 +0200

    Upload

--- Lab Functions and Logic flow/02_Temperature-Converter/solution.js
+++ Lab Functions and Logic flow/02_Temperature-Converter/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  // TODO
+    let degrees = Number(document.getElementById('num1').value);
+    let type = document.getElementById('type').value;
+    let result = '';
+    let converted = 0;
+    let correctType = false;
+
+    function convert(degrees, type) {
+        if (type.toLowerCase() === 'fahrenheit') {
+            converted = (((degrees - 32) * 5) / 9);
+            correctType = true;
+        } else if (type.toLowerCase() === 'celsius') {
+            converted = ((degrees * 9) / 5) + 32;
+            correctType = true;
+        }
+    }
+
+    function print() {
+        if (correctType) {
+            result = Math.round(converted);
+        } else {
+            result = 'Error!';
+        }
+    }
+
+    convert(degrees, type);
+    print();
+    document.getElementById('result').innerHTML = result;
 }
--- Lab Functions and Logic flow/03_Count-Occurrences-of-a-Given-Character/solution.js
+++ Lab Functions and Logic flow/03_Count-Occurrences-of-a-Given-Character/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  // TODO
+    let string = document.getElementById('string').value;
+    let char = document.getElementById('character').value;
+    let result = '';
+    let charCount = 0;
+    let resultElement = document.getElementById('result');
+
+    function findCharOccurences(string, char) {
+        for (let i = 0; i < string.length; i++) {
+            if (string[i] === char) {
+                charCount++;
+            }
+        }
+    }
+
+    function evenOrOdd() {
+        if (charCount % 2 === 0) {
+            result = `Count of ${char} is even.`;
+        } else {
+            result = `Count of ${char} is odd.`;
+        }
+    }
+
+    findCharOccurences(string, char);
+    evenOrOdd();
+    resultElement.innerHTML = result;
+
 }
