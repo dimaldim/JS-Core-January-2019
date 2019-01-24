d708399dfe72f76bb7b6d2c1708e377acfc8e73c
Dimitar <antraks92@gmail.com>
2019-01-24 15:18:45 +0200

    Upload

--- Exercise Functions and Logic Flow/04_EGN-Validator/solution.js
+++ Exercise Functions and Logic Flow/04_EGN-Validator/solution.js
@@ -3,95 +3,66 @@
     button.addEventListener('click', generateEGN);
 
     function generateEGN() {
-        let year = document.getElementById('year');
-        let month = document.getElementById('month');
-        let day = document.getElementById('date');
-        let region = document.getElementById('region');
-        let sex = document.getElementById('male').checked ? 1 : ((document.getElementById('female').checked) ? (2) : undefined);
-        let weights = [2, 4, 8, 5, 10, 9, 7, 3, 6];
         let egn;
-
-        let validYear = Number(year.value) >= 1900 && Number(year.value) <= 2100;
-        let validRegion = Number(region.value) >= 43 && Number(region.value) <= 999;
-
-        if (validYear && month.value && day.value && validRegion && sex) {
-
-            let gregion = String(region.value.slice(0, 2)) + random(0, 9);
-
-            if (sex === 1 && (gregion % 2 !== 0)) {
-                gregion--;
-            } else if (sex === 2 && (gregion % 2 === 0)) {
-                gregion++;
-            }
-            let lastDigit;
-            egn = year.value.slice(-2) + getMonth(month.value) + (day.value < 10 ? '0' + day.value : day.value) + gregion;
-            let weightSum = () => {
-                let sum = 0;
-                for (i = 0; i < egn.length; i++) {
-                    sum += Number(egn[i]) * weights[i];
-                }
-                return sum;
+        let weights = [2, 4, 8, 5, 10, 9, 7, 3, 6];
+        var getYear = () => document.getElementById('year').value;
+        var getMonth = () => {
+            months = {
+                January: '01',
+                February: '02',
+                March: '03',
+                April: '04',
+                May: '05',
+                June: '06',
+                July: '07',
+                August: '08',
+                September: '09',
+                October: '10',
+                November: '11',
+                December: '12',
             };
-            let checkNum = weightSum() % 11;
-            if (checkNum === 10) {
-                egn += '0';
-            } else {
-                egn += checkNum;
+            return months[document.getElementById('month').value];
+        };
+        var getDay = () => ('0' + (document.getElementById('date').value)).slice(-2);
+        var getGender = () => {
+            return document.getElementById('male').checked ? 1 :
+                ((document.getElementById('female').checked) ? (2) : undefined);
+        };
+        var getRegion = () => ('0' + (document.getElementById('region').value)).slice(-3);
+
+        var regionAndGender = (region, gender) => {
+            region = region.slice(0, 2);
+            for (let i = 1; i <= 9; i++) {
+                if (gender === 1 && i % 2 === 0) {
+                    return region + i;
+                } else if (gender === 2 && i % 2 !== 0) {
+                    return region + i;
+                }
             }
-            document.getElementById('egn').textContent = `Your EGN is: ${egn}`;
 
-            year.value = '';
-            month.value = '';
-            day.value = '';
-            region.value = '';
+        };
 
+        if (getYear() && getMonth() && getDay() && getGender() && getRegion() &&
+            getYear() >= 1900 && getYear() <= 2100 && getRegion() >= 43 && getRegion() <= 999) {
+
+            egn = (getYear()).slice(2) + getMonth() + getDay() + regionAndGender(getRegion(), getGender());
+            let egnSum = 0;
+            for (let i = 0; i < 9; i++) {
+                egnSum += egn[i] * weights[i];
+            }
+            let validChecksum = egnSum % 11;
+            if (validChecksum === 10) {
+                validChecksum = 0;
+            }
+            egn += validChecksum;
+
+            document.getElementById('egn').innerHTML = `Your EGN is: ${egn}`;
+            document.getElementById('year').value = '';
+            document.getElementById('date').value = '';
+            document.getElementById('region').value = '';
+            document.getElementById('month').selectedIndex = 0;
+            document.getElementById('male').checked = false;
+            document.getElementById('female').checked = false;
         }
-    }
-
-    function random(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    function getMonth(name) {
-        let month;
-        switch (name) {
-            case 'January':
-                month = '01';
-                break;
-            case 'February':
-                month = '02';
-                break;
-            case 'March':
-                month = '03';
-                break;
-            case 'April':
-                month = '04';
-                break;
-            case 'May':
-                month = '05';
-                break;
-            case 'June':
-                month = '06';
-                break;
-            case 'July':
-                month = '07';
-                break;
-            case 'August':
-                month = '08';
-                break;
-            case 'September':
-                month = '09';
-                break;
-            case 'October':
-                month = '10';
-                break;
-            case 'November':
-                month = '11';
-                break;
-            case 'December':
-                month = '12';
-                break;
-        }
-        return month;
     }
 })();
