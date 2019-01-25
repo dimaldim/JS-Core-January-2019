70309ef5d28801f42f8b4362d853420283303622
Dimitar <antraks92@gmail.com>
2019-01-25 09:27:27 +0200

    Upload

--- Lab Arrays and Matrices/02_Even-Position-Element/solution.js
+++ Lab Arrays and Matrices/02_Even-Position-Element/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  // TODO
+    let input = JSON.parse(document.getElementById('arr').value);
+    let resultElement = document.getElementById('result');
+    let result = [];
+
+    var findEven = (arr) => {
+        arr.forEach((element, index) => {
+            if (index % 2 === 0) {
+                result.push(element);
+            }
+        });
+    };
+
+    findEven(input);
+    resultElement.innerHTML = result.join(' x ');
 }
