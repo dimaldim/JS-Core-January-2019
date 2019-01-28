c936d732861b04bba589911c123d6ad9d30a0d7b
Dimitar <antraks92@gmail.com>
2019-01-28 09:28:57 +0200

    Upload

---
+++ Exercise Arrays and Matrices/6.Sort an Array by 2 Criteria.js
@@ -0,0 +1,9 @@
+function solve(input) {
+    input.sort((a, b) => {
+        if (a.length === b.length) {
+            return a < b ? -1 : 1;
+        }
+        return a.length - b.length;
+    })
+    console.log(input.join('\n'));
+}
