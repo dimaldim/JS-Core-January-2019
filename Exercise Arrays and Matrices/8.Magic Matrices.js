f2f149ada629037249d2db46a49151b507ba47b1
Dimitar <antraks92@gmail.com>
2019-01-28 10:01:18 +0200

    Upload

--- Exercise Arrays and Matrices/8.Magic Matrices.js
+++ Exercise Arrays and Matrices/8.Magic Matrices.js
@@ -4,13 +4,13 @@ function solve(matrix) {
     for (let row = 1; row < matrix.length; row++) {
         let rowSum = 0;
         matrix[row].forEach(x => rowSum += x);
-        if (rowSum != sum) return false;
+        if (rowSum !== sum) return false;
     }
     for (let col = 0; col < matrix[0].length; col++) {
         let colSum = 0;
         for (let row = 0; row < matrix.length; row++)
             colSum += matrix[row][col];
-        if (colSum != sum) return false;
+        if (colSum !== sum) return false;
     }
     return true;
 }
