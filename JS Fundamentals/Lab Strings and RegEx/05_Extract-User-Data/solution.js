11470b094f42ba1a2731632a313b5db7333ff14a
Dimitar <antraks92@gmail.com>
2019-01-29 15:12:48 +0200

    Upload

--- JS Fundamentals/Lab Strings and RegEx/05_Extract-User-Data/solution.js
+++ JS Fundamentals/Lab Strings and RegEx/05_Extract-User-Data/solution.js
@@ -1,13 +1,10 @@
 function solve() {
-
     let arr = JSON.parse(document.getElementById("arr").value);
-
     let result = document.getElementById('result');
 
     function userData(arr) {
 
-        let pattern = /^([A-Z][a-z]* [A-Z][a-z]*) (\+359 [0-9] [0-9]{3}[0-9]{3}|\+359-[0-9]-[0-9]{3}-[0-9]{3}) ([a-z0-9]+@[a-z]+\.[a-z]{2,3})$/;
-
+        let pattern = /^([A-Z][a-z]* [A-Z][a-z]*) (\+359 [0-9] [0-9]{3} [0-9]{3}|\+359-[0-9]-[0-9]{3}-[0-9]{3}) ([a-z0-9]+@[a-z]+\.[a-z]{2,3})$/;
         let match;
 
         for (let data of arr) {
@@ -17,29 +14,18 @@ function solve() {
             if (match) {
 
                 let firstParagraph = document.createElement('p');
-
                 firstParagraph.textContent = `Name: ${match[1]}`;
-
                 result.appendChild(firstParagraph);
-
                 let secondParagraph = document.createElement('p');
-
                 secondParagraph.textContent = `Phone Number: ${match[2]}`;
-
                 result.appendChild(secondParagraph);
-
                 let thirdParagraph = document.createElement('p');
-
                 thirdParagraph.textContent = `Email: ${match[3]}`;
-
                 result.appendChild(thirdParagraph);
-
             } else {
 
                 let errorParagraph = document.createElement('p');
-
                 errorParagraph.textContent = 'Invalid data';
-
                 result.appendChild(errorParagraph);
 
             }
@@ -50,5 +36,6 @@ function solve() {
             result.appendChild(dashes);
         }
     }
+
     userData(arr);
 }
