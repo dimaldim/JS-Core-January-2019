e4e3e8cb98797069851d3a1779074bb1dc628233
Dimitar <antraks92@gmail.com>
2019-02-15 10:25:39 +0200

    Upload

--- JS Fundamentals/Exercise Objects and JSON/02.Furniture/solution.js
+++ JS Fundamentals/Exercise Objects and JSON/02.Furniture/solution.js
@@ -1,60 +1,51 @@
 function solve() {
-//[{"name": "Sofa", "img": "https://res.cloudinary.com/maisonsdumonde/image/upload/q_auto,f_auto/w_200/img/grey-3-seater-sofa-bed-200-13-0-175521_9.jpg", "price": 150, "decFactor": 1.2}]
+    let someList = [];
+    let totalPrice = 0;
+    let decFactors = [];
 
-    let outputElement = document.getElementById('furniture-list');
-    let generateBtn = Array.from(document.querySelectorAll('button'))[0];
-    let buyBtn = Array.from(document.querySelectorAll('button'))[1];
-    let resultElement = Array.from(document.querySelectorAll('textarea'))[1];
-
-    generateBtn.addEventListener('click', generate);
-    buyBtn.addEventListener('click', buy);
-
-    function buy() {
-        let checked = Array.from(document.querySelectorAll("input:checked"));
-        let furnitures = [];
-        let totalSum = 0;
-        let avgFactor = 0;
-
-        checked.forEach(item => {
-            furnitures.push(item.parentNode.children[0].textContent.split('Name: ').filter(x => x !== '')[0]);
-            totalSum += Number(item.parentNode.children[2].textContent.split('Price: ').filter(x => x !== '')[0]);
-            avgFactor += Number(item.parentNode.children[3].textContent.split('Decoration factor: ').filter(x => x !== '')[0]);
-        });
-        let avgResult = avgFactor / furnitures.length;
-
-
-        resultElement.textContent = `Bought furniture: ${furnitures.join(', ')}\n`;
-        resultElement.textContent += `Total price: ${totalSum.toFixed(2)}\n`;
-        resultElement.textContent += `Average decoration factor: ${avgResult}\n`;
-    }
-
-    function generate() {
-        let input = document.querySelector('textarea').value;
-        let objects = JSON.parse(input);
-
-        for (obj of objects) {
+    document.getElementsByTagName('button')[0].addEventListener('click', () => {
+        let furnitureList = JSON.parse(document.getElementsByTagName('textarea')[0].value);
+        for (let furniture of furnitureList) {
             let div = document.createElement('div');
-            let pName = document.createElement('p');
-            let pPrice = document.createElement('p');
-            let pFactor = document.createElement('p');
-            let img = document.createElement('img');
-            let checkbox = document.createElement('input');
-
             div.setAttribute('class', 'furniture');
-            pName.innerHTML = `Name: ${obj.name}`;
-            pPrice.innerHTML = `Price: ${obj.price}`;
-            pFactor.innerHTML = `Decoration factor: ${obj.decFactor}`;
-            img.src = obj.img;
-            checkbox.type = 'checkbox';
+            let name = document.createElement('p');
+            name.innerHTML = `Name: ${furniture.name}`;
+            let img = document.createElement('img');
+            img.setAttribute('src', furniture.img);
+            let price = document.createElement('p');
+            price.innerHTML = `Price: ${furniture.price}`;
+            let decFactor = document.createElement('p');
+            decFactor.innerHTML = `Decoration factor: ${furniture.decFactor}`;
+            let checkbox = document.createElement('input');
+            checkbox.setAttribute('type', 'checkbox');
 
-            div.appendChild(pName);
+            div.appendChild(name);
             div.appendChild(img);
-            div.appendChild(pPrice);
-            div.appendChild(pFactor);
+            div.appendChild(price);
+            div.appendChild(decFactor);
             div.appendChild(checkbox);
 
-            outputElement.appendChild(div);
+            document.getElementById('furniture-list').appendChild(div);
         }
-    }
+    })
 
+    document.getElementsByTagName('button')[1].addEventListener('click', () => {
+        let array = Array.from(document.getElementById('furniture-list').children);
+        for (let furniture of array) {
+            let isChecked = furniture.getElementsByTagName('input')[0].checked;
+            if (isChecked) {
+                let name = furniture.getElementsByTagName('p')[0].innerHTML.split(': ')[1];
+                someList.push(name);
+                let price = +furniture.getElementsByTagName('p')[1].innerHTML.split(': ')[1];
+                totalPrice += price;
+                let decFactor = +furniture.getElementsByTagName('p')[2].innerHTML.split(': ')[1];
+                decFactors.push(decFactor);
+            }
+        }
+        let textarea = document.getElementsByTagName('textarea')[1];
+        textarea.value += `Bought furniture: ${someList.join(', ')}\n`;
+        textarea.value += `Total price: ${totalPrice.toFixed(2)}\n`;
+        let averageDecFac = decFactors.reduce((a, b) => a + b) / decFactors.length;
+        textarea.value += `Average decoration factor: ${averageDecFac}`;
+    })
 }
