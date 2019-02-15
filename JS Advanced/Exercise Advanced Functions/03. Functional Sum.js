d78ad9708fcc9d0a94e50a6adad16d6720003827
Dimitar <antraks92@gmail.com>
2019-02-15 11:03:55 +0200

    Upload

---
+++ JS Advanced/Exercise Advanced Functions/03. Functional Sum.js
@@ -0,0 +1,14 @@
+function add(num) {
+    let sum = num;
+
+    function sumNum(num2) {
+        sum += num2;
+        return sumNum;
+    }
+
+    sumNum.toString = function () {
+        return sum;
+    };
+
+    return sumNum;
+}
