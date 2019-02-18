5408ba6699f81d2d7f5a88a177c284ea3c60a2d9
Dimitar <antraks92@gmail.com>
2019-02-18 15:37:37 +0200

    Upload

--- JS Advanced/Lab JS Classes/05. Point Distance.js
+++ JS Advanced/Lab JS Classes/05. Point Distance.js
@@ -1,1 +1,10 @@
-//todo
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(a, b) {
+        return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+    }
+}
