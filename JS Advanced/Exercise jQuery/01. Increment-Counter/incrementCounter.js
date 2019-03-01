6a85217e62b6c8c3afa2bd86e4662e1a243966d0
Dimitar <antraks92@gmail.com>
2019-03-01 15:59:58 +0200

    Fixed increment counter

--- JS Advanced/Exercise jQuery/01. Increment-Counter/incrementCounter.js
+++ JS Advanced/Exercise jQuery/01. Increment-Counter/incrementCounter.js
@@ -1,40 +1,40 @@
 function increment(selector)
 {
-    let container = $(selector);
-    let fragment = document.createDocumentFragment();
-    let textArea = $('<textarea>');
-    let incrBtn = $('<button>Increment</button>');
-    let addBtn = $('<button>Add</button>');
-    let list = $('<ul>');
+	let container = $(selector);
+	let fragment = document.createDocumentFragment();
+	let textArea = $('<textarea>');
+	let incrBtn = $('<button>Increment</button>');
+	let addBtn = $('<button>Add</button>');
+	let list = $('<ul>');
 
-    textArea.addClass('counter');
-    textArea.val('0');
-    textArea.attr('disabled', 'true');
+	textArea.addClass('counter');
+	textArea.val('0');
+	textArea.attr('disabled', 'true');
 
-    incrBtn.addClass('btn');
-    incrBtn.attr('id', 'incrementBtn');
+	incrBtn.addClass('btn');
+	incrBtn.attr('id', 'incrementBtn');
 
-    addBtn.addClass('btn');
-    addBtn.attr('id', 'addBtn');
+	addBtn.addClass('btn');
+	addBtn.attr('id', 'addBtn');
 
-    list.addClass('results');
+	list.addClass('results');
 
-    $(incrBtn).on('click', function ()
-    {
-        textArea.val(+textArea.val() + 1);
-    });
+	$(incrBtn).on('click', function ()
+	{
+		textArea.val(+textArea.val() + 1);
+	});
 
-    (addBtn).on('click', function ()
-    {
-        let li = $(`<li>${textArea.val()}</li>`);
-        li.appendTo(list);
-    });
+	$(addBtn).on('click', function ()
+	{
+		let li = $(`<li>${textArea.val()}</li>`);
+		li.appendTo(list);
+	});
 
 
-    textArea.appendTo(fragment);
-    incrBtn.appendTo(fragment);
-    addBtn.appendTo(fragment);
-    list.appendTo(fragment);
+	textArea.appendTo(fragment);
+	incrBtn.appendTo(fragment);
+	addBtn.appendTo(fragment);
+	list.appendTo(fragment);
 
-    container.append(fragment);
+	container.append(fragment);
 }
