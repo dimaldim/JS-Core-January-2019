5abd5bc454321b7c7c3a81aa17bbe7f077439e08
Dimitar <antraks92@gmail.com>
2019-03-01 16:13:03 +0200

    Upload

--- JS Advanced/Lab Object Composition/02. Fibonacci.js
+++ JS Advanced/Lab Object Composition/02. Fibonacci.js
@@ -1,11 +1,14 @@
-function getFibonator() {
-    let sum, first = 0;
-    let second = 1;
+function getFibonator()
+{
+	let sum, first = 0;
+	let second = 1;
 
-    return () => {
-        sum = first + second;
-        first = second;
-        second = sum;
-        return first;
-    }
+	return () =>
+	{
+		sum = first + second;
+		first = second;
+		second = sum;
+		return first;
+	}
 }
+let a = 'b';
