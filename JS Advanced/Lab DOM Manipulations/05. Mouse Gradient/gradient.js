34f32306639c2ddcfe29781b9713026034934bd1
Dimitar <antraks92@gmail.com>
2019-03-05 14:52:06 +0200

    Upload

---
+++ JS Advanced/Lab DOM Manipulations/05. Mouse Gradient/gradient.js
@@ -0,0 +1,16 @@
+function attachGradientEvents()
+{
+		let $gradient = $('#gradient');
+		let $result = $('#result');
+
+		$gradient.on('mousemove', (e) =>
+		{
+				let rectangle = e.target.getBoundingClientRect();
+
+				let y = parseInt(e.offsetX);
+				let x = parseInt(rectangle.left);
+				let result = Math.floor((y - x) * 100 / 300);
+
+				$result.text(`${result}%`);
+		});
+}
