c6901fee88403beee241796bf04290da8cd3747b
dimaldim <antraks92@gmail.com>
2019-04-05 16:56:31 +0300

    Upload

--- JS Applications/Templating Exercises/Popular-Monkeys/monkeysTemplate.js
+++ JS Applications/Templating Exercises/Popular-Monkeys/monkeysTemplate.js
@@ -1,3 +1,28 @@
-$(() => {
-    // TODO
-})
+$(() =>
+{
+	render();
+
+	function render()
+	{
+		const source = $('#monkey-template').html();
+		const template = Handlebars.compile(source);
+		const html = template({monkeys});
+
+		$('.monkeys').html(html);
+
+		$('button').on('click', function (e)
+		{
+			let btn = $(e.target);
+			let p = btn.next();
+			if (p.attr('style'))
+			{
+				p.removeAttr('style');
+				btn.text('Hide info');
+			} else
+			{
+				p.css('display', 'none');
+				btn.text('Info');
+			}
+		})
+	}
+});
